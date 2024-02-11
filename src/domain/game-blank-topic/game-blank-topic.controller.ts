48a7d6680fd64a51a13e3837f623ad4ddc8bf12d
hyebin <xxico89@gmail.com>
2024-02-11 23:55:14 +0900

    :sparkles: feat(domain): 빈칸주제 질문지 조회하기 API 추가
    ISSUES CLOSED: #30

--- src/domain/game-blank-topic/game-blank-topic.controller.ts
+++ src/domain/game-blank-topic/game-blank-topic.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Res } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { GameBlankTopicService } from './game-blank-topic.service';
+import { Response } from 'express';
 
-@Controller('game-blank-topic')
-export class GameBlankTopicController {}
+@ApiTags('Blank Topic API')
+@Controller('game/blank-topic')
+export class GameBlankTopicController {
+        constructor(private readonly gameBlankTopicService: GameBlankTopicService) {}
+
+        @ApiOperation({
+                summary: '빈칸 주제 질문지 조회하기 API',
+                description: '빈칸 주제 게임의 질문지를 조회합니다.',
+        })
+        @ApiResponse({
+                status: 200,
+                description: '빈칸 주제 질문지 조회 성공',
+        })
+        @Get()
+        async getBlankTopic(@Res() res: Response) {
+                const question = await this.gameBlankTopicService.getBlankQuestion();
+                return res.status(HttpStatus.OK).json(question);
+        }
+}
--- src/domain/game-blank-topic/game-blank-topic.service.ts
+++ src/domain/game-blank-topic/game-blank-topic.service.ts
@@ -1,4 +1,17 @@
+import { PrismaService } from '@/shared-service/prisma';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class GameBlankTopicService {}
+export class GameBlankTopicService {
+        constructor(private readonly prismaService: PrismaService) {}
+
+        async getBlankQuestion() {
+                let lastQId = 0; // 임시 question id
+                // NOTE: 각 방마다 마지막으로 진행한 question id 필드 필요성 논의 필요 -> room id를 전달받아서 question id를 필터링 해야 할 것 같음
+                const question = this.prismaService.gameBlankTopic.findMany({
+                        select: { id: true, description: true },
+                        where: { id: lastQId + 1 },
+                });
+                return question;
+        }
+}
