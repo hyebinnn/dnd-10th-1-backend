b056f1d8b3953db22c53dd5769e04dd274929779
hyebin <xxico89@gmail.com>
2024-02-16 03:51:23 +0900

    :sparkles: feat(domain): 사용자 프로필 설정 API 추가
    ISSUES CLOSED: #24

--- src/domain/user/user.service.ts
+++ src/domain/user/user.service.ts
@@ -1,4 +1,27 @@
+import { PrismaService } from '@/shared-service/prisma';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+        constructor(private readonly prismaService: PrismaService) {}
+
+        async setUserProfile(nickName: string, profileImage: string, roomId: string) {
+                if (roomId === '') {
+                        await this.prismaService.user.create({
+                                data: {
+                                        displayName: nickName,
+                                        profileImage: profileImage,
+                                        role: 'Owner',
+                                },
+                        });
+                } else {
+                        await this.prismaService.user.create({
+                                data: {
+                                        displayName: nickName,
+                                        profileImage: profileImage,
+                                        role: 'Participant',
+                                },
+                        });
+                }
+        }
+}
